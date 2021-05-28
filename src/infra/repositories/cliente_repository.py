9dd1a1f530e3543e228e853f8e35f031d7ca8dfa
Manoel Vieira <manoelv8@gmail.com>
2021-05-28 15:38:26 -0300

    Update cliente_repository.py

--- src/infra/repositories/cliente_repository.py
+++ src/infra/repositories/cliente_repository.py
@@ -1,26 +1,26 @@
 from src.infra.config import DBConnetionHandler
-from src.infra.entities import Clientes
-from collections import namedtuple
+from src.infra.entities import Clientes as clienteModel
+from src.domain.models import Clientes
 
 
 class ClienteRepository:
     """Cliente Repository"""
 
     @classmethod
-    def insert_cliente(cls, nome: str, email: str) -> Clientes:
+    def insert_cliente(cls, nome: str, email: str) -> ClienteModel:
         """
         Inserindo um cliente na base de dados
         :param nome: Nome do cliente
         :param email: Email do cliente
         :return Tupla com um cliente
         """
-        InsertData = namedtuple("Clientes", "id, nome, email")
+        
         with DBConnetionHandler() as db_connection:
             try:
-                cliente = Clientes(nome=nome, email=email)
+                cliente = ClienteModel(nome=nome, email=email)
                 db_connection.session.add(cliente)
                 db_connection.session.commit()
-                return InsertData(id=cliente.id, nome=cliente.nome, email=cliente.email)
+                return Clientes(id=cliente.id, nome=cliente.nome, email=cliente.email)
             except:
                 db_connection.session.rollback()
                 raise
